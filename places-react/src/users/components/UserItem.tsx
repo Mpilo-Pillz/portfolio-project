365ceeeaf24887687df31f18a60f89b70e71aa61
Fikasentani <mpilo.dlamini@yahoo.com>
2022-09-15 02:31:23 +0200

    updating user item to accept links

--- places-react/src/users/components/UserItem.tsx
+++ places-react/src/users/components/UserItem.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../../shared/components/UIElements/Avatar";
 
 import "./UserItem.css";
 
@@ -12,15 +14,17 @@ const UserItem: React.FC<UserItemProps> = ({ id, image, name, placeCount }) => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>
-            {placeCount} {placeCount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
       </div>
     </li>
   );
