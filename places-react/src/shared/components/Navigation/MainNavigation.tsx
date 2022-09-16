5c118a04d89c2777522882e3f6807089dd1b2391
Fikasentani <mpilo.dlamini@yahoo.com>
2022-09-16 18:11:03 +0200

    adding annimation

--- places-react/src/shared/components/Navigation/MainNavigation.tsx
+++ places-react/src/shared/components/Navigation/MainNavigation.tsx
@@ -10,25 +10,26 @@ import Backdrop from "../UIElements/Backdrop";
 const MainNavigation = () => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  const openDrawer = () => {
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
   };
 
-  const closeDrawer = () => {
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
   };
   return (
     <>
-      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-      {drawerIsOpen && (
-        <SideDrawer>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav>
-        </SideDrawer>
-      )}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer onClick={closeDrawerHandler} show={drawerIsOpen}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
--- places-react/src/shared/components/Navigation/SideDrawer.tsx
+++ places-react/src/shared/components/Navigation/SideDrawer.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { CSSTransition } from "react-transition-group";
+
 import "./SideDrawer.css";
 
 interface SideDrawerProps {
+  show: boolean;
+  onClick: () => void;
   children: React.ReactNode;
 }
-const SideDrawer: React.FC<SideDrawerProps> = ({ children }) => {
-  const content = <aside className="side-drawer">{children}</aside>;
+const SideDrawer: React.FC<SideDrawerProps> = ({ show, children, onClick }) => {
+  const content = (
+    <CSSTransition
+      in={show}
+      timeout={2}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={onClick}>
+        {children}
+      </aside>
+    </CSSTransition>
+  );
 
   return ReactDOM.createPortal(
     content,
