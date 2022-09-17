0d8be4b20d0eb866aeb098b3b450d9032104c4b5
Fikasentani <mpilo.dlamini@yahoo.com>
2022-09-18 01:28:58 +0200

    creating a dynamic modal

---
+++ places-react/src/shared/components/UIElements/Modal.tsx
@@ -0,0 +1,72 @@
+import React, { CSSProperties } from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+import Backdrop from "./Backdrop";
+
+import "./Modal.css";
+
+interface ModalOverlayProps {
+  children?: React.ReactNode;
+  footer?: React.ReactNode;
+  className?: string;
+  style?: CSSProperties | undefined;
+  headerClass?: string;
+  contentClass?: string;
+  footerClass?: string;
+  header?: string;
+  onSubmit?: () => void;
+
+  // TODO - revise this type
+  show?: boolean;
+  onCancel?: () => void;
+}
+
+interface ModalProps {
+  show: boolean;
+  onCancel: () => void;
+}
+
+const ModalOverlay: React.FC<ModalOverlayProps> = (props) => {
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
+  return ReactDOM.createPortal(
+    content,
+    document.getElementById("modal-hook") as HTMLElement
+  );
+};
+
+const Modal: React.FC<ModalOverlayProps> = (props) => {
+  return (
+    <>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </>
+  );
+};
+
+export default Modal;
