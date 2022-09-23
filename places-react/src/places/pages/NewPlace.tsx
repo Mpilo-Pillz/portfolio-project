f77b4f64da6537eca88afdeedb5f80035beb3f3a
Fikasentani <mpilo.dlamini@yahoo.com>
2022-09-23 12:12:45 +0200

    adding input handler to new place

--- places-react/src/places/pages/NewPlace.tsx
+++ places-react/src/places/pages/NewPlace.tsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 
 import "./NewPlace.css";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback(
+    (id: string, value: string, isValid: boolean) => {},
+    []
+  );
+
+  const descriptionInputHandler = useCallback(
+    (id: string, value: string, isValid: boolean) => {},
+    []
+  );
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         type="text"
         label="Title"
-        id="title"
         element="input"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+
+      <Input
+        label="Title"
+        id="description"
+        element="textarea"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters) "
+        onInput={descriptionInputHandler}
       />
     </form>
   );
