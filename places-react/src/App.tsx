7d287b230fd2855d71ac2b490622ef3de7948b33
Fikasentani <mpilo.dlamini@yahoo.com>
2022-09-26 16:12:45 +0200

    adding update place component

--- places-react/src/App.tsx
+++ places-react/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import UserPlace from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./users/pages/Users";
 
@@ -11,8 +12,9 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Users />} />
-          <Route path="/:userId/places" element={<UserPlaces />} />
+          <Route path="/:userId/places" element={<UserPlace />} />
           <Route path="/places/new" element={<NewPlace />} />
+          <Route path="/places/:placeId" element={<UpdatePlace />} />
           <Route
             path="*"
             element={<Navigate to="/" replace />} //this is a way to redirect
---
+++ places-react/src/places/pages/UpdatePlace.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import Button from "../../shared/components/FormElements/Button";
+import Input from "../../shared/components/FormElements/Input";
+import { DUMMY_PLACES } from "../../shared/mockData/dummyPlaces";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min. 5 characters)"
+        onInput={() => {}}
+        value={identifiedPlace.description}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
