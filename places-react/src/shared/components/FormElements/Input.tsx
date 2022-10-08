588a0630b7e245b32124ef5ff3a3c2da259feba3
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-08 13:14:45 +0200

    Making input props more readable

--- places-react/src/shared/components/FormElements/Input.tsx
+++ places-react/src/shared/components/FormElements/Input.tsx
@@ -13,8 +13,8 @@ interface InputProps {
   element: string;
   errorText: string;
   validators: InputValidator[];
-  value?: string;
-  valid?: boolean;
+  initialValue?: string;
+  initialValidity?: boolean;
   onInput: (id: string, inputStateVaue: string, isValid: boolean) => any;
 }
 
@@ -52,9 +52,9 @@ const Input: React.FC<InputProps> = ({
   // arg2 is the initial state
   // receives current state and updates it based on action received and retruns new state
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: props.value || "",
+    value: props.initialValue || "",
     isTouched: false,
-    isValid: props.valid || false,
+    isValid: props.initialValidity || false,
   });
 
   const { value, isValid } = inputState;
