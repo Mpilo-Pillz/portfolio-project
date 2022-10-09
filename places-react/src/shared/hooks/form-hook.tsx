68127e6145b92ef63a4ec813905ff8896d85ad1a
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-09 09:32:28 +0200

    adding Set data reducer

--- places-react/src/shared/hooks/form-hook.tsx
+++ places-react/src/shared/hooks/form-hook.tsx
@@ -6,10 +6,14 @@ const formReducer = (state: any, action: formActionState) => {
   // console.log("STATE-->", state);
   switch (action.type) {
     case "INPUT_CHANGE":
-      let formIsValid = true;
+      let formIsValid = true; // option 2
+      // let formIsValid: boolean | undefined = true; // option 3
       for (const inputId in state.inputs) {
         if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
+          formIsValid = formIsValid && (action.isValid as boolean); // option 2
+          // formIsValid = formIsValid && action.isValid; // option 3
+          // formIsValid = formIsValid && action.isValid!; // will never be null. I think it might becuase of the other dispatch
+          // or just remove the optional the cos the issue is action.isValid might be undefiend
         } else {
           formIsValid = formIsValid && state.inputs[inputId].isValid;
         }
@@ -21,6 +25,11 @@ const formReducer = (state: any, action: formActionState) => {
           [action.inputId]: { value: action.value, isValid: action.isValid },
         },
         isValid: formIsValid,
+      };
+    case "SET_DATA":
+      return {
+        inputs: action.inputs,
+        isValid: action.formIsValid,
       };
     default:
       return state;
@@ -43,7 +52,16 @@ export const useForm = (
     []
   );
 
-  return [formState, inputHandler];
+  // Let react store it so we do not recreate it unnecesarily
+  const setFormData = useCallback((inputData: any, formValidity: boolean) => {
+    dispatch({
+      type: "SET_DATA",
+      inputs: inputData,
+      formIsValid: formValidity,
+    });
+  }, []);
+
+  return [formState, inputHandler, setFormData];
 };
 
 export default useForm;
