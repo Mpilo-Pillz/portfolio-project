9c46df0a0ff7a25d0c630bbd3d009a9a1734c594
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-11 15:30:38 +0200

    Conditionally rendering items if logged in and adding auth context

--- places-react/src/App.tsx
+++ places-react/src/App.tsx
@@ -1,14 +1,26 @@
+import { useCallback, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlace from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 import Auth from "./users/pages/Auth";
 import Users from "./users/pages/Users";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, [setIsLoggedIn]);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, [setIsLoggedIn]);
+
   return (
-    <>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
       <MainNavigation />
       <main>
         <Routes>
@@ -23,7 +35,7 @@ const App = () => {
           />
         </Routes>
       </main>
-    </>
+    </AuthContext.Provider>
   );
 };
 
