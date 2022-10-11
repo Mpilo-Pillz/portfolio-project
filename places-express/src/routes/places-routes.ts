bc7b4da2bafd5d16c6f101c186d290cf14f7213d
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-11 19:44:43 +0200

    adding dummy data and filtering the requests from the url

--- places-express/src/routes/places-routes.ts
+++ places-express/src/routes/places-routes.ts
@@ -2,9 +2,39 @@ import express, { Router } from "express";
 
 const placesRoutes = Router();
 
-placesRoutes.get("/", (req, res, next) => {
-  console.log("GET REQUEST in places");
-  res.json({ message: "From Route" });
+export const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Mbabane",
+    description: "One of the places I grew up",
+    address: "Dr Sishayi, Mbabane, Eswatini",
+    location: {
+      lat: -26.3152423,
+      lng: 31.1196755,
+    },
+    creator: "u1",
+    imageUrl:
+      "https://images.unsplash.com/photo-1655207882298-bd11bb69ee43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGVzd2F0aW5pfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: "p2",
+    title: "Mlilwane",
+    description: "My first Cycle Trail",
+    address: "Lobamba, Eswatini",
+    location: {
+      lat: -26.4798342,
+      lng: 31.1938053,
+    },
+    creator: "u2",
+    imageUrl:
+      "https://images.unsplash.com/photo-1652478412734-22ff7ad45dee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGVzd2F0aW5pfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
+
+placesRoutes.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  res.json({ place });
 });
 
 export default placesRoutes;
