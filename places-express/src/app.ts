8836ee9d0f24d654c275978f27275b921dcf3542
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-14 19:39:51 +0200

    adding error handling for routes that are not found

--- places-express/src/app.ts
+++ places-express/src/app.ts
@@ -2,13 +2,20 @@ import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 
 import placesRoutes from "./routes/places-routes";
-import { ErrorResponse } from "./models/http-error";
+import HttpError, { ErrorResponse } from "./models/http-error";
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not found this route", 404);
+
+  throw error;
+});
+
 app.use(
   (error: ErrorResponse, req: Request, res: Response, next: NextFunction) => {
     if (res.headersSent) {
