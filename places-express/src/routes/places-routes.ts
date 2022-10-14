2d4473993db392d31fc12d3572818dc380ef9815
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-14 19:25:35 +0200

    registering place route to place controller

--- places-express/src/routes/places-routes.ts
+++ places-express/src/routes/places-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  createPlace,
   getPlaceById,
   getPlaceByUserId,
 } from "../controllers/places-controller";
@@ -9,5 +10,7 @@ const placesRoutes = Router();
 placesRoutes.get("/:pid", getPlaceById);
 
 placesRoutes.get("/user/:uid", getPlaceByUserId);
+
+placesRoutes.post("/", createPlace);
 
 export default placesRoutes;
