73f4836457781d91dd2cd9bb889b7204a1134662
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-19 12:12:45 +0200

    adding delete and patch route

--- places-express/src/routes/places-routes.ts
+++ places-express/src/routes/places-routes.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import {
   createPlace,
+  deletePlace,
   getPlaceById,
-  getPlaceByUserId,
+  getPlacesByUserId,
+  updatePlace,
 } from "../controllers/places-controller";
 
 const placesRoutes = Router();
 
 placesRoutes.get("/:pid", getPlaceById);
-
-placesRoutes.get("/user/:uid", getPlaceByUserId);
-
+placesRoutes.get("/user/:uid", getPlacesByUserId);
 placesRoutes.post("/", createPlace);
+placesRoutes.patch("/:pid", updatePlace);
+placesRoutes.delete("/:pid", deletePlace);
 
 export default placesRoutes;
