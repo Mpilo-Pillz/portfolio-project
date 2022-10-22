815996238b846237e6646f126ce4962823ffaea1
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-22 13:12:45 +0200

    adding express middle ware to controller

--- places-express/src/routes/places-routes.ts
+++ places-express/src/routes/places-routes.ts
@@ -6,12 +6,21 @@ import {
   getPlacesByUserId,
   updatePlace,
 } from "../controllers/places-controller";
+import { check } from "express-validator";
 
 const placesRoutes = Router();
 
 placesRoutes.get("/:pid", getPlaceById);
 placesRoutes.get("/user/:uid", getPlacesByUserId);
-placesRoutes.post("/", createPlace);
+placesRoutes.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  createPlace
+);
 placesRoutes.patch("/:pid", updatePlace);
 placesRoutes.delete("/:pid", deletePlace);
 
