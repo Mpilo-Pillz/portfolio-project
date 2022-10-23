257c357427c72798397c96b8890f8d5b492c1411
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-23 11:05:59 +0200

    adding location util

---
+++ places-express/util/location.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import HttpError from "../src/models/http-error";
+
+const API_KEY = "";
+
+export async function getCoordsForAddress(address: string): Promise<{
+  lat: number;
+  lng: number;
+}> {
+  return {
+    lat: -26.3152423,
+    lng: 31.1196755,
+  };
+
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}}`
+  );
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422
+    );
+
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
+}
