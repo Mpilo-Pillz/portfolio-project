149efa75cefaac2415f41e1faa63c15ca2ff7a6e
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-23 08:25:32 +0200

    adding validation to sign up route

--- places-express/src/routes/users-route.ts
+++ places-express/src/routes/users-route.ts
@@ -1,10 +1,21 @@
 import express, { Router } from "express";
 import { getUsers, login, signup } from "../controllers/users-controller";
+import { check } from "express-validator";
 
 const usersRoutes = Router();
 
 usersRoutes.get("/", getUsers);
-usersRoutes.post("/signup", signup);
+usersRoutes.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  signup
+);
 usersRoutes.post("/login", login);
 
 export default usersRoutes;
+
+// normalisedEmail lower cases the email eg Test@testing.com becomes test@testing.com
