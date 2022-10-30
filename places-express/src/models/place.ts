99015c1b0c20e01c8a6601c57b6a189c4b70d4a0
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-30 12:55:34 +0200

    creating place schema

---
+++ places-express/src/models/place.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+  title: { type: String, require: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  creator: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Place", placeSchema);
