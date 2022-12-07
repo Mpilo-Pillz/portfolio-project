d95cd76953898b50fc9de4b3ab26f84ee2bc5883
Fikasentani <mpilo.dlamini@yahoo.com>
2022-12-07 13:05:52 +0200

    creating a custom http hook

---
+++ places-react/src/shared/hooks/http-hook.tsx
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+type httpRequestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>();
+
+  // prevent the state change after the page has been moved from the DOM
+  // cancel teh connected request
+  const activeHttpRequests = useRef<AbortController[]>([]);
+
+  const sendRequest = useCallback(
+    async (
+      url: string,
+      method: httpRequestMethod = "GET",
+      body = null,
+      headers = {}
+    ) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData: { message: string } = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        return responseData;
+      } catch (err: any) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
