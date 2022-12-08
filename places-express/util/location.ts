38687d2c3d631a299c53332c24fc2d6abd46c460
Fikasentani <mpilo.dlamini@yahoo.com>
2022-12-08 21:32:40 +0200

    enabling cal to google maps

--- places-express/util/location.ts
+++ places-express/util/location.ts
@@ -1,22 +1,25 @@
+import * as dotenv from "dotenv";
 import axios from "axios";
 import HttpError from "../src/models/http-error";
 
-const API_KEY = "";
+dotenv.config();
 
 export async function getCoordsForAddress(address: string): Promise<{
   lat: number;
   lng: number;
 }> {
-  return {
-    lat: -26.3152423,
-    lng: 31.1196755,
-  };
+  // return {
+  //   lat: -26.3152423,
+  //   lng: 31.1196755,
+  // };
 
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
-    )}&key=${API_KEY}}`
+    )}&key=${process.env.GOOGLE_MAPS_API_KEY}`
   );
+  console.log("response-->", response);
+
   const data = response.data;
 
   if (!data || data.status === "ZERO_RESULTS") {
