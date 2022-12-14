d8ed1704b560f081287765801d2cd02e6b6b9311
Fikasentani <mpilo.dlamini@yahoo.com>
2022-12-14 20:49:15 +0200

    feat: WIP reading place mage form server

--- places-express/src/controllers/places-controller.ts
+++ places-express/src/controllers/places-controller.ts
@@ -134,8 +134,7 @@ export const createPlace = async (
     description,
     address,
     location: coordinates,
-    image:
-      "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    image: req.file?.path,
     creator,
   });
 
