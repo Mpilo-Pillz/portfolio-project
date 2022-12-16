13b5a036030c356fbb95f547cc713deec13d8711
Fikasentani <mpilo.dlamini@yahoo.com>
2022-12-16 21:03:31 +0200

    Authentication: saving authentication information to local staorage

--- places-react/src/App.tsx
+++ places-react/src/App.tsx
@@ -15,6 +15,7 @@ const App = () => {
   const login = useCallback(
     (uid: string, token?: string) => {
       setToken(token!);
+      localStorage.setItem("userData", JSON.stringify({ userId: uid, token }));
       setUserId(uid);
     },
     [setToken]
